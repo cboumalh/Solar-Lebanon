eca361ecf4168697ea0e700f4c3df5d5b36fa12c
Chris Boumalhab <84485659+cboumalh@users.noreply.github.com>
2022-07-03 20:57:05 -0400

    Add files via upload

---
+++ solar-energy-frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header/header';
+import Data from './components/data/data';
+import Footer from './components/footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      < Header/>
+      < Data/>
+      < Footer/>
+    </div>
+  );
+}
+
+export default App;
