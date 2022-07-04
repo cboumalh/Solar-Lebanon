dcf29b6aebf3ca4c13cf135704613298a96fa5e0
Chris Boumalhab <84485659+cboumalh@users.noreply.github.com>
2022-07-03 20:54:49 -0400

    Add files via upload

---
+++ solar-energy-frontend/src/components/map/map.js
@@ -0,0 +1,9 @@
+import './map.css'
+
+export default function Map(){
+    return(
+        <div className='Map'>
+            <img src={require('../../assets/lebanon-map.png')} alt='map'/>
+        </div>
+    );
+}
---
+++ solar-energy-frontend/src/components/map/map.test.js
@@ -0,0 +1,16 @@
+import Map from "./map";
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+
+describe('testing Map component', ()=>{
+    test('map compontent renders', ()=>{
+        render(< Map/>)
+    })
+
+    test('map image is there', ()=>{
+        render(< Map/>)
+
+        const image = screen.getByAltText('map');
+        expect(image).toHaveAttribute('src', 'lebanon-map.png')
+    })
+
+})
